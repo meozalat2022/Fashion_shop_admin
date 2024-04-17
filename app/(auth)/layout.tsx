ab65a397cf050f5fb1ffd25131e607d5f2041107
meozalat2022 <meozalat2002@hotmail.com>
2024-04-17 16:04:48 +0200

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fashion App - Admin Auth",
+  description: "Fashion Shop Admin Dashboard to Manage Application Data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
